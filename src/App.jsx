4c0267743d514a98622ba399b3d0c5af7bc3e0d0
Alex Liew <liewtwoo42@gmail.com>
2023-08-31 17:26:27 -0400

    get pokemon information to transfer properly to recharts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import {useState, useEffect} from "react"
 import AllPokemons from './components/allPokemons.jsx'
+import {BarChart, Bar, XAxis, YAxis} from 'recharts'
 
 function App() {
   const [loadPoke, setLoadPoke] = useState('https://pokeapi.co/api/v2/pokemon?limit=5')
   const [pokemonInformation, setPokemonInformation] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   const getPokemons = async() => {
     const data = await fetch(loadPoke)
@@ -16,19 +18,35 @@ function App() {
         const info = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
         const res = await info.json()
         setPokemonInformation(currentList => [...currentList, res])
+        setIsLoading(false)
       })
     }
     createPokemonObject(response.results)
     await console.log(pokemonInformation)
   }
 
-
   useEffect(() => {
     getPokemons()
   }, [])
 
+  if (isLoading) {
+    return (
+      <div>
+        This page is still loading
+      </div>
+    )
+  }
+
+ 
+
   return(
     <div className='container'>
+      {console.log(pokemonInformation)}
+      <BarChart width={600} height={300} data={pokemonInformation[0].stats}>
+        <Bar dataKey="base_stat" barSize={30} fill="#8884d8"/> 
+        <XAxis dataKey='stat["name"]'/>
+        <YAxis />
+      </BarChart>
       <h1>POKEMON!!</h1>
       <div className="pokemon" style={{display: 'flex', flexFlow: 'row wrap', padding:'20px'}}>
           {pokemonInformation.map((pokemon, index) => 
@@ -40,6 +58,12 @@ function App() {
             type={pokemon.types}
             height={pokemon.height}
             weight={pokemon.weight}
+            stat1={pokemon.stats[0].base_stat}
+            stat2={pokemon.stats[1].base_stat}
+            stat3={pokemon.stats[2].base_stat}
+            stat4={pokemon.stats[3].base_stat}
+            stat5={pokemon.stats[4].base_stat}
+            stat6={pokemon.stats[5].base_stat}
             
           />
         )}
--- src/components/AllPokemons.jsx
+++ src/components/AllPokemons.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-const AllPokemons = ({id,name,image,type,height,weight}) => {
+
+const AllPokemons = ({id,name,image,type,height,weight,stat1,stat2,stat3,stat4,stat5,stat6}) => {
 
   let pokemon_type = ""
   if (type[1] === undefined) {
@@ -19,7 +20,7 @@ const AllPokemons = ({id,name,image,type,height,weight}) => {
       <ul className="list-group list-group-flush">
         <li className="list-group-item">Height: {height/10} meters <br/>
         Weight: {weight/10} kg</li>
-        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">{stat1} {stat2} {stat3} {stat4} {stat5} {stat6}</li>
         <li className="list-group-item">A third item</li>
       </ul>
       <div className="card-body">
